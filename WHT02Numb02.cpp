f0c0f4a67f07494df4c224cd6b486dace7e8cbe3
Kornpituk <43356993+Kornpituk@users.noreply.github.com>
2018-09-24 11:04:21 +0700

    Add files via upload

---
+++ WHT02Numb02.cpp
@@ -0,0 +1,63 @@
+
+#include<stdio.h>
+
+int main (){
+	int Number1,Number2,Number3;
+	printf("Enter Number1:");
+	scanf("%d",&Number1);
+	printf("Enter Number2:");
+	scanf("%d",&Number2);
+	printf("Enter Number3:");
+	scanf("%d",&Number3);
+	//Max&&Min-----------------------------------------------------------------------**Max&&Min**--------------------------------------------------------------------------
+	if(Number1>Number2 && Number1>Number3||Number1>Number3&&Number2>Number3&&Number1>Number2){
+		printf("Number Max: %d\n",Number1);
+	}
+	else if(Number2>Number1 && Number2>Number3){
+		printf("Number Max: %d\n",Number2);
+	}
+	else if(Number3>Number1 && Number3>Number2){
+		printf("Number Max: %d\n",Number3);
+	}
+	else if(Number1>2&&Number3>2&&Number1>3){
+		printf("Number Max :%d\n",Number1);
+	}
+	else if(Number1>2&&Number3>2&&Number1<3){
+		printf("Number Max :%d\n",Number3);
+	}
+	//---------------------MaxEnd----------------------------
+	 if(Number1<Number2 && Number1<Number3 ||Number1==Number3 ||Number1==Number2){
+		printf("Number Min: %d\n",Number1);
+	}
+	else if(Number2<Number1 && Number2<Number3||Number2==Number3||Number2==1){
+		printf("Number Min: %d\n",Number2);
+	}
+	else if(Number3<Number1 && Number3<Number2||Number3==Number2||Number3==Number1){
+		printf("Number Min: %d\n",Number3);
+	}//Min&&Max------------------------------------------------------------------**EndMin&&Max**----------------------------------------------------------------
+	//StartEqual---------------------------------------------------------------------------**StartEqual**------------------------------------------------------------------
+	if(Number1==Number2&&Number2!=Number3){
+		printf("Number equal %d\n",Number1,Number2);
+	}
+	else if(Number1==Number3&&Number3!=Number2){
+		printf("Number equal %d\n",Number1,Number3);
+	}
+	else if(Number2==Number3&&Number3!=Number1){
+		printf("Number equal %d\n",Number2,Number3);
+	}
+	else if(Number2==Number1&&Number1!=Number3){
+		printf("Number equal %d\n",Number2,Number1);
+	}
+	else if(Number2==Number1&&Number1==Number3&&Number3==Number2){
+		printf("Number equal: %d,%d,and %d\n",Number2,Number1,Number3);
+	}
+	else if(Number1!=Number2&&Number2!=Number3&&Number3!=Number1){
+		printf("Not Number Equal!\n");
+	}
+	else {
+		printf("ProGram Error!!!\n");
+	}
+	//EndEqual-------------------------------------------------------------------------------------------------------------------------------------------
+	//StartAndMore------------------------------------------------------------------------------------------------------------
+	return 0;
+}
