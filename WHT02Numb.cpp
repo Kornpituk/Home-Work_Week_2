dc309bdd529d64af3e9245526d80772f6f1e4a5c
Kornpituk <43356993+Kornpituk@users.noreply.github.com>
2018-09-24 11:08:23 +0700

    Add files via upload

--- WHT02Numb.cpp
+++ WHT02Numb.cpp
@@ -10,7 +10,7 @@ int main (){
 	printf("Enter Number3:");
 	scanf("%d",&Number3);
 	//Max&&Min-----------------------------------------------------------------------**Max&&Min**--------------------------------------------------------------------------
-	if(Number1>Number2 && Number1>Number3){
+	if(Number1>Number2 && Number1>Number3||Number1>Number3&&Number2>Number3&&Number1>Number2){
 		printf("Number Max: %d\n",Number1);
 	}
 	else if(Number2>Number1 && Number2>Number3){
@@ -25,52 +25,14 @@ int main (){
 	else if(Number1>2&&Number3>2&&Number1<3){
 		printf("Number Max :%d\n",Number3);
 	}
-	else if(Number1>Number3&&Number2>Number3&&Number1>Number2){
-		printf("Number Max :%d\n",Number1);
-	}
-	else if(Number1>Number3&&Number2>Number3&&Number1<Number2){
-		printf("Number Max :%d\n",Number2);
-	}
-	else if(Number1>Number2&&Number3>Number2&&Number1>=Number3){
-		printf("Numebr Max :%d\n",Number1);
-	}
-	else if(Number1>Number2&&Number3>Number2&&Number1<=Number3){
-		printf("Number Max :%d\n");
-	}
-	//end NUmber1
-	else if(Number2>Number1&&Number3>Number1&&Number2>=Number3){
-		printf("Number Max :%d\n",Number2);
-	}
-	else if(Number2>Number1&&Number3>Number1&&Number2<=Number3){
-		printf("Number Max :%d\n",Number3);
-	}
-	else if(Number2>Number3&&Number1>Number3&&Number2>=Number1){
-		printf("Number Max :%d\n",Number2);
-	}
-	else if(Number2>Number3&&Number1>Number3&&Number2<=Number1){
-		printf("Number Max :%d\n",Number1);
-	}
-	//End Number2
-	else if(Number3>Number1&&Number2>Number1&&Number3>=Number2){
-		printf("Number Max :%d\n",Number3);
-	}
-	else if(Number3>Number1&&Number2>Number1&&Number3<=Number2){
-		printf("Number Max :%d",Number2);
-	}
-	else if(Number3>2&&Number1>Number2&&Number3>=Number1){
-		printf("Number Max :%d\n",Number1);
-	}
-	else if(Number3>Number2&&Number1>Number2&&Number3<=Number2){
-		printf("Number Max :%d\n",Number2);
-	}//end Number3 Max
 	//---------------------MaxEnd----------------------------
-	 if(Number1<Number2 && Number1<Number3||Number1<Number2&&Number1<=Number3||Number1<Number3&&Number1>=Number2){
+	 if(Number1<Number2 && Number1<Number3 ||Number1==Number3 ||Number1==Number2){
 		printf("Number Min: %d\n",Number1);
 	}
-	else if(Number2<Number1 && Number2<Number3||Number2<Number1&&Number2<=Number3){
+	else if(Number2<Number1 && Number2<Number3||Number2==Number3||Number2==1){
 		printf("Number Min: %d\n",Number2);
 	}
-	else if(Number3<Number1 && Number3<Number2||Number3<Number1&&Number3<=Number2){
+	else if(Number3<Number1 && Number3<Number2||Number3==Number2||Number3==Number1){
 		printf("Number Min: %d\n",Number3);
 	}//Min&&Max------------------------------------------------------------------**EndMin&&Max**----------------------------------------------------------------
 	//StartEqual---------------------------------------------------------------------------**StartEqual**------------------------------------------------------------------
